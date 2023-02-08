09564975b8c5f9cc95a17b48cadfc447fe8abf83
Andrew Gallagher <andrew@acgallagher.com>
2023-02-08 15:51:52 -0500

    Finished 4.3.1

--- week4/de_zoomcamp/models/staging/stg_yellow_tripdata.sql
+++ week4/de_zoomcamp/models/staging/stg_yellow_tripdata.sql
@@ -2,21 +2,21 @@
 
 SELECT
     -- identifiers
-    {{ dbt_utils.generate_surrogate_key(['vendorid','lpep_pickup_datetime']) }} AS trip_id,
+    {{ dbt_utils.generate_surrogate_key(['vendorid','tpep_pickup_datetime']) }} AS trip_id,
     CAST(vendorid AS INTEGER) AS vendorid,
     CAST(ratecodeid AS INTEGER) AS ratecodeid,
     CAST(pulocationid AS INTEGER) AS pulocationid,
     CAST(dolocationid AS INTEGER) AS dolocationid,
 
     -- timestamps
-    CAST(lpep_pickup_datetime AS TIMESTAMP) AS pickup_datetime,
-    CAST(lpep_dropoff_datetime AS TIMESTAMP) AS dropoff_datetime,
+    CAST(tpep_pickup_datetime AS TIMESTAMP) AS pickup_datetime,
+    CAST(tpep_dropoff_datetime AS TIMESTAMP) AS dropoff_datetime,
 
     -- trip info
     store_and_fwd_flag,
     CAST(passenger_count AS INTEGER) AS passenger_count,
     CAST(trip_distance AS NUMERIC) AS trip_distance,
-    CAST(trip_type AS INTEGER) AS trip_type,
+    1 AS trip_type,
 
     -- payment info
     CAST(fare_amount AS NUMERIC) AS fare_amount,
@@ -24,14 +24,14 @@ SELECT
     CAST(mta_tax AS NUMERIC) AS mta_tax,
     CAST(tip_amount AS NUMERIC) AS tip_amount,
     CAST(tolls_amount AS NUMERIC) AS tolls_amount,
-    CAST(ehail_fee AS INTEGER) AS ehail_fee,
+    0 AS ehail_fee,
     CAST(improvement_surcharge AS NUMERIC) AS improvement_surcharge,
     CAST(total_amount AS NUMERIC) AS total_amount,
     CAST(payment_type AS INTEGER) AS payment_type,
     {{ get_payment_type_description('payment_type') }} AS payment_type_description,
     CAST(congestion_surcharge AS NUMERIC) AS congestion_surcharge,
 
-FROM {{ source('staging', 'green_tripdata') }}
+FROM {{ source('staging', 'yellow_tripdata') }}
 WHERE vendorid IS NOT NULL
 -- dbt build --n <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
